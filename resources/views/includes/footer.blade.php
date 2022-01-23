71fcd292e36a5879c5abd480d78c31dc9d7baefc
Nipuna Theekshana <nipunatheekshana8@gmail.com>
2022-01-23 20:12:30 +0530

    deleted all

--- resources/views/includes/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="content-footer">
-    <div>© {{ date('Y') }} - <a href="" target="_blank">Nipuna Theekshana</a></div>
-    <div>
-        <nav class="nav">
-            <a href="#" class="nav-link">Get Help</a>
-        </nav>
-    </div>
-</footer>
